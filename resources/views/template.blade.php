c74525469a2e7942c9bef3971db628484ac8aba9
Юра <maine679@gmail.com>
2022-02-13 19:26:16 +0200

    up

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -39,14 +39,13 @@
             <h1>Enter your vehicle details to receive a free valuation.</h1>
             <p>If your vehicle has <b>less than 100,000 miles</b> enter your vehicle and contact details and we will contact you with a cash valuation within 24 hours.</p>
         </div>
-        <form name="" class="form-registration" method="" action="">
+        <form name="" class="form-registration" method="" action="#">
                 <input id="registration" type="text" placeholder="ENTER REG" size="40">
 
             <div class="checkboxes">
                 <div class="checkbox-group"><input type="checkbox" class="checkbox"><label for="checkbox-1" class="label">I confirm that the vehicle I am enquiring about has less than 100,000 miles.</label></div>
             </div>
-
-            <p><input class="button-reg" type="submit" value="Отправить"></p>
+            <button class="button-reg" type="submit" value="Отправить"><i class="fa fa-arrow"><b>Get Started<b></b></i></button>
         </form>
     </div>
 </div>
