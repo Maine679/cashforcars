98f377eb419b93f95463d22a459970a2764c40f1
Юра <maine679@gmail.com>
2022-02-13 19:10:17 +0200

    Написание первого экрана

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -30,69 +30,31 @@
     </div>
 </header>
 
-<div class="wrapper">
+<div class="first-screen">
 
-    <div class="block-1">
-        <h1>Enter your vehicle details to receive a free valuation.</h1>
-        <p>If your vehicle has <b>less than 100,000 miles</b> enter your vehicle and contact details and we will contact you with a cash valuation within 24 hours.</p>
+
+    <div class="wrapper">
+
+        <div class="for-details">
+            <h1>Enter your vehicle details to receive a free valuation.</h1>
+            <p>If your vehicle has <b>less than 100,000 miles</b> enter your vehicle and contact details and we will contact you with a cash valuation within 24 hours.</p>
+        </div>
+        <form name="" class="form-registration" method="" action="">
+                <input id="registration" type="text" placeholder="ENTER REG" size="40">
+
+            <div class="checkboxes">
+                <div class="checkbox-group"><input type="checkbox" class="checkbox"><label for="checkbox-1" class="label">I confirm that the vehicle I am enquiring about has less than 100,000 miles.</label></div>
+            </div>
+
+            <p><input class="button-reg" type="submit" value="Отправить"></p>
+        </form>
     </div>
-    <div class="block-2">Corporis, sint?</div>
-    <div class="test_class">Maxime, omnis?</div>
-    <div class="test_class">Libero, tenetur?</div>
-    <div class="test_class">Repudiandae, sunt!</div>
-    <div class="test_class">Architecto, iusto!</div>
-    <div class="test_class">Quibusdam, voluptatem.</div>
-    <div class="test_class">Consectetur, debitis.</div>
-    <div class="test_class">Accusantium, maiores.</div>
-    <div class="test_class">Aliquid, culpa?</div>
-    <div class="test_class">Aperiam, esse.</div>
-    <div class="test_class">Perferendis, sequi.</div>
-    <div class="test_class">Porro, saepe.</div>
-    <div class="test_class">Facilis, sit?</div>
-    <div class="test_class">Inventore, quod!</div>
-    <div class="test_class">Assumenda, temporibus?</div>
-    <div class="test_class">Ad, voluptates?</div>
-    <div class="test_class">Deserunt, totam.</div>
-    <div class="test_class">Recusandae, sunt.</div>
-    <div class="test_class">Expedita, numquam.</div>
-    <div class="test_class">Quae, vel!</div>
-    <div class="test_class">Nobis, provident.</div>
-    <div class="test_class">Adipisci, eum.</div>
-    <div class="test_class">Hic, id?</div>
-    <div class="test_class">Est, recusandae?</div>
-    <div class="test_class">Fuga, reprehenderit.</div>
-    <div class="test_class">Minima, quae?</div>
-    <div class="test_class">Dicta, eum!</div>
-    <div class="test_class">Accusamus, distinctio.</div>
-    <div class="test_class">Adipisci, delectus.</div>
-    <div class="test_class">Aliquam, velit!</div>
-    <div class="test_class">Enim, optio.</div>
-    <div class="test_class">Excepturi, saepe.</div>
-    <div class="test_class">Impedit, reiciendis.</div>
-    <div class="test_class">Dolores, officia?</div>
-    <div class="test_class">Ipsa, minus!</div>
-    <div class="test_class">Cupiditate, voluptate?</div>
-    <div class="test_class">Quod, veniam?</div>
-    <div class="test_class">Earum, tenetur.</div>
-    <div class="test_class">Odit, sapiente.</div>
-    <div class="test_class">Assumenda, itaque!</div>
-    <div class="test_class">Ipsum, reiciendis!</div>
-    <div class="test_class">Maiores, sit!</div>
-    <div class="test_class">Blanditiis, inventore.</div>
-    <div class="test_class">Iste, repudiandae.</div>
-    <div class="test_class">Autem, dolore?</div>
-    <div class="test_class">Eveniet, nobis.</div>
-    <div class="test_class">Quibusdam, veniam!</div>
-    <div class="test_class">Fugit, veritatis.</div>
-    <div class="test_class">Cumque, eveniet.</div>
-
 </div>
-<style>
-</style>
+
+
 
 @yield('content')
 
-footer
-</div>
+
 </body>
 </html>
